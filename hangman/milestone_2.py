bd53d4e895861724cfd4060083a22ed94f9797dc
CJ1608 <128046995+CJ1608@users.noreply.github.com>
2023-09-12 12:13:37 +0100

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,27 +1,31 @@
 import random
 
-def create_list():
-    my_list = ['Apple', 'Banana', 'Melon', 'Mango', 'Kiwi']
-    word = random.choices(my_list)
-    
-    print(my_list, word)
+def create_fruit_list():
+    my_fruit_list = ['Apple', 'Banana', 'Melon', 'Mango', 'Kiwi']
+    word_from_fruit_list = random.choices(my_fruit_list)
 
 
 def receive_user_letter():
-    #finish loop only when have a valid guess
-    guessed = False
+    """
+    Prompt user for letter and continue looping until letter is accepted (user input should be one alphabetical letter). 
     
-    while guessed == False:
-        #make it obvious to user what expected
+    Non-numerical, length of 1 and no special characters dependent on user input.
+    Code removes whitespaces and makes input lowercase to reduce errors in if else loop. 
+
+    No return, just print statements to user. 
+    """
+    guess_accepted = False
+    
+    while guess_accepted == False:
         print("\nPlease enter a single letter. For example: a. \nPlease do not include numbers or special characters.\n")
-        #strip white space and casefold to help if loop
-        guess = input("Please enter your chosen single letter: ").strip().casefold() 
         
-        if (len(guess) == 1) and (guess.isalpha()):
-            print(f'Good guess: {guess}')
-            guessed = True
-        elif (len(guess) != 1) or (guess.isalpha()==False):
-            print(f'Bad guess: {guess}')
+        user_guess = input("Please enter your chosen single letter: ").strip().casefold() 
+        
+        if (len(user_guess) == 1) and (user_guess.isalpha()):
+            print(f'Good guess: {user_guess}')
+            guess_accepted = True
+        elif (len(user_guess) != 1) or (user_guess.isalpha()==False):
+            print(f'Bad guess: {user_guess}')
         #else statement hopefully won't ever run but might make easier to catch errors...
         else:
             print('Error')
